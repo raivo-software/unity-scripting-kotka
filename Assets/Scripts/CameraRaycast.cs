ea0a651f9f90810321196fb4bd6a928762803cc4
Teemu Salminen <teemujsa@gmail.com>
2019-10-15 15:33:31 +0300

    Add raycasting based destruction for spheres

---
+++ Assets/Scripts/CameraRaycast.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour
+{
+    public Collider LeftSphere;
+    public Collider RightSphere;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            bool hitSuccesful = Physics.Raycast(ray, out hitInfo);
+            if(hitSuccesful)
+            {
+                Collider other = hitInfo.collider;
+                if (other == LeftSphere)
+                {
+                    Destroy(other.gameObject);
+                }
+                if (other == RightSphere)
+                {
+                    Destroy(other.gameObject);
+                }
+            }
+        }
+    }
+}
