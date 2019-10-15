ac76c0d4457265af1f20138b8fb2c4b0399bb170
Teemu Salminen <teemujsa@gmail.com>
2019-10-15 09:56:28 +0300

    Add movement controls to spheres

---
+++ Assets/Scripts/BounceAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceAnimation : MonoBehaviour
+{
+    private Vector3 movement = Vector3.up;
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > 1)
+        {
+            movement = Vector3.down;
+        }
+        else if(transform.position.y < -1)
+        {
+            movement = Vector3.up;
+        }
+        transform.Translate(movement * Time.deltaTime);
+    }
+}
--- Assets/Scripts/DebugComponent.cs
+++ Assets/Scripts/DebugComponent.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class DebugComponent : MonoBehaviour
 {
-    public GameObject Sphere;
+    public GameObject Sphere1;
+    public GameObject Sphere2;
+    public GameObject Sphere3;
+
+    private GameObject TargetSphere;
+
     private Vector3 movement = Vector3.zero;
 
     // Start is called before the first frame update
@@ -16,7 +21,7 @@ public class DebugComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
         if(Input.GetKeyDown("w"))
         {
             movement = movement + Vector3.forward;
@@ -34,6 +39,31 @@ public class DebugComponent : MonoBehaviour
             movement = movement - Vector3.back;
         }
 
-        Sphere.transform.Translate((movement * 5) * Time.deltaTime);
+        if(TargetSphere != null)
+        {
+            TargetSphere.transform.Translate((movement * 5) * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown("1") && Sphere1.GetComponents<BounceAnimation>().Length == 0)
+        {
+            Sphere1.SetActive(true);
+            Sphere2.SetActive(false);
+            Sphere3.SetActive(false);
+            TargetSphere = Sphere1;
+        }
+        if (Input.GetKeyDown("2") && Sphere2.GetComponents<BounceAnimation>().Length == 0)
+        {
+            Sphere2.SetActive(true);
+            Sphere1.SetActive(false);
+            Sphere3.SetActive(false);
+            TargetSphere = Sphere2;
+        }
+        if (Input.GetKeyDown("3") && Sphere3.GetComponents<BounceAnimation>().Length == 0)
+        {
+            Sphere3.SetActive(true);
+            Sphere2.SetActive(false);
+            Sphere1.SetActive(false);
+            TargetSphere = Sphere3;
+        }
     }
 }
