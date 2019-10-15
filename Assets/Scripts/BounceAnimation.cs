586b905a49ff8d1863881e758ef0aef63b3a8ada
Teemu Salminen <teemujsa@gmail.com>
2019-10-15 14:36:47 +0300

    Collide with left and right spheres

--- Assets/Scripts/BounceAnimation.cs
+++ Assets/Scripts/BounceAnimation.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class BounceAnimation : MonoBehaviour
 {
-    private Vector3 movement = Vector3.up;
+    public Collider LeftSphere;
+    public Collider RightSphere;
+
+    private Vector3 movement = Vector3.left;
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y > 1)
+        if(transform.position.x > 1)
         {
-            movement = Vector3.down;
+            movement = Vector3.left;
         }
-        else if(transform.position.y < -1)
+        else if(transform.position.x < -1)
         {
-            movement = Vector3.up;
+            movement = Vector3.right;
         }
         transform.Translate(movement * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == LeftSphere)
+            Debug.Log("Collided with left sphere");
+        else if (other == RightSphere)
+            Debug.Log("Collided with right sphere");
     }
 }
--- Assets/Scripts/DebugComponent.cs
+++ Assets/Scripts/DebugComponent.cs
@@ -4,88 +4,12 @@ using UnityEngine;
 
 public class DebugComponent : MonoBehaviour
 {
-    public Transform Parent;
-    private GameObject Sphere1;
-    private GameObject Sphere2;
-    private GameObject Sphere3;
 
-    private GameObject TargetSphere;
-
-    private Vector3 movement = Vector3.zero;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Starting script");
-        Sphere1 = GameObject.FindWithTag("Sphere");
-        Sphere2 = GameObject.FindWithTag("Sphere2");
-        Sphere3 = GameObject.FindWithTag("Sphere3");
-        Sphere1.transform.SetParent(Parent);
-        Sphere2.transform.SetParent(Parent);
-        Sphere3.transform.SetParent(Parent);
-    }
+    public GameObject TargetSphere;
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKeyDown("w"))
-        {
-            movement = movement + Vector3.forward;
-        }
-        if(Input.GetKeyUp("w"))
-        {
-            movement = movement - Vector3.forward;
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            movement = movement + Vector3.back;
-        }
-        if (Input.GetKeyUp("s"))
-        {
-            movement = movement - Vector3.back;
-        }
 
-        if(TargetSphere != null)
-        {
-            TargetSphere.transform.Translate((movement * 5) * Time.deltaTime);
-        }
-
-        if(Input.GetKeyDown("1"))
-        {
-            Sphere1.SetActive(true);
-            Sphere2.SetActive(false);
-            Sphere3.SetActive(false);
-            TargetSphere = Sphere1;
-
-            if(Sphere1.GetComponents<BounceAnimation>().Length == 0)
-            {
-                Sphere1.AddComponent<BounceAnimation>();
-            }
-        }
-        if (Input.GetKeyDown("2"))
-        {
-            Sphere2.SetActive(true);
-            Sphere1.SetActive(false);
-            Sphere3.SetActive(false);
-            TargetSphere = Sphere2;
-
-            if (Sphere2.GetComponents<BounceAnimation>().Length == 0)
-            {
-                Sphere2.AddComponent<BounceAnimation>();
-            }
-        }
-        if (Input.GetKeyDown("3"))
-        {
-            Sphere3.SetActive(true);
-            Sphere2.SetActive(false);
-            Sphere1.SetActive(false);
-            TargetSphere = Sphere3;
-
-            if (Sphere3.GetComponents<BounceAnimation>().Length == 0)
-            {
-                Sphere3.AddComponent<BounceAnimation>();
-            }
-        }
     }
 }
